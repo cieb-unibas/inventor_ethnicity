5438f3ea5b52a6b683078fa33c5de49d2f995b7b
MatthNig <matthias.niggli@unibas.ch>
2021-11-29 15:27:29 +0100

    added raw data for .js plots

--- data/01_data_creation.R
+++ data/01_data_creation.R
@@ -3,7 +3,7 @@
 #                 the EPO to investigate the ethnicity of inventors   #
 #                 across countries and over time.                     #
 # Authors:        Matthias Niggli/CIEB University of Basel            #
-# Last Revised:   25.11.2021                                          #
+# Last Revised:   29.11.2021                                          #
 #######################################################################
 
 ##################################################
@@ -11,12 +11,12 @@
 ##################################################
 
 #### directories  -----------------------------------------------------------------
-DatDir <- "/scicore/home/weder/GROUP/Innovation/01_patent_data"
 if(substr(x = getwd(), 
           nchar(getwd())-17, nchar(getwd())) == "inventor_ethnicity"){
   print("Working directory corresponds to repository directory")}else{
     print("Make sure your working directory is the repository directory.")}
 # setwd(paste0(getwd(), "/Innoscape-GitHub-Repos/inventor_ethnicity"))
+DatDir <- "/scicore/home/weder/GROUP/Innovation/01_patent_data"
 
 #### Load libraries --------------------------------------------
 library("tidyverse")
@@ -41,6 +41,15 @@ print("Data on technological groups loaded")
 
 #### merge information together ---------------------------------
 inv_dat <- merge(inv_dat, techfield_grouping, by = "tech_field", all.x = TRUE)
+
+##### stocks of immigrant and resident inventors per country ---------------------------------
+# data is taken from: 
+# Miguelez, Ernest, and Fink, Carsten,(2013) “Measuring the international mobility of inventors: a new database” WIPO Economic Research Working Paper No. 8.
+immig_dat <- read.csv(
+  paste0(
+    DatDir,
+    "/raw data/wipo_inventor_nationality_database/raw_files/13. Stocks of nationals, immigrants, residents, emigrants.csv")
+)
 
 #### Load helper functions --------------------------------------------
 source("data/02_data_creation_funs.R")
@@ -69,7 +78,12 @@ if(length(unique(plot1_df$country)) == length(COUNTRIES)){
     warning("Number of countries in the created data does not correspond to function input.")}
 
 #### Add country name
-plot2_df$country_name <- countrycode(plot2_df$country, "iso2c", "country.name.en")
+plot1_df$country_name <- countrycode(plot1_df$country, "iso2c", "country.name.en")
+
+#### Order the countries
+tmp <- data.frame(country_name = unique(plot1_df$country_name), country_id = c(1,2,9,3,5,6,7,8,4,11,12,10))
+plot1_df <- merge(plot1_df, tmp, all.x=TRUE, by = "country_name")
+tmp <- NULL
 
 #### visualize the data
 ggplot(plot1_df, aes(x = p_year, y = share, color = origin))+
@@ -124,19 +138,47 @@ ggplot(plot2_df, aes(x = p_year, y = share, color = country_name))+
         axis.line = element_line(),
         axis.title = element_text(face="bold",size=10))
 
+##############################################
+######## Figure 3: Immigrant Stocks ##########
+##############################################
+
+#### subset to the time window and countries of the analysis
+TIME_WINDOW <- range(immig_dat$prio_year)
+plot3_df <- immig_dat %>% 
+  filter(iso_alpha2_code %in% unique(plot1_df$country) &
+           prio_year %in% TIME_WINDOW)
+
+#### select the data
+plot3_df <- plot3_df %>% 
+  group_by(iso_alpha2_code) %>% arrange(prio_year) %>%
+  summarize(immigrant_difference = diff(immigrants)) %>%
+  merge(plot3_df, all.x = TRUE, by = "iso_alpha2_code") %>%
+  select(iso_alpha2_code, prio_year, immigrants, immigrant_difference) %>%
+  rename(year = prio_year)
+            
+#### Add country name
+plot3_df$country_name <- countrycode(plot3_df$iso_alpha2_code, "iso2c", "country.name.en")
+
+#### visualize the data
+ggplot(plot3_df, aes(x = country_name, y = immigrants, fill = as.factor(year)))+
+  geom_col(position = "dodge")+
+  labs(x = "Country", y = "Number of Immigrants") +
+  theme(panel.background = element_blank(),
+        panel.grid.major.y = element_line(linetype = "dotted", color = "grey"),
+        legend.position = "bottom",
+        axis.line = element_line(),
+        axis.title = element_text(face="bold",size=10))
+
 ##################################
 ######## Save the datasets #######
 ##################################
 
-for (i in seq(2)){
+for (i in seq(3)){
   dat_name <- paste0("plot", i, "_df")
   write.csv(get(dat_name),
             paste0("report/", dat_name, ".csv"),
             row.names = FALSE)
   print(paste("Data for plot", i, "saved."))
 }
-
-# write.csv(x = plot2_df, file = "report/plot2_df.csv", row.names = FALSE)
-# print("Data creation completed.")
 
 
